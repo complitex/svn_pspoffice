bcc28c2fad8998605efe00cd9babb5c8d9846400
artkof.work <artkof.work@38c073cf-b0f8-bd99-4753-81ba8f8cd33b>
2010-08-10 06:30:18 +0000

    all book's strategies and functional was moved to information module.

---
+++ trunk/src/passportoffice-information/src/main/java/org/complitex/pspoffice/information/strategy/region/RegionStrategy.java
@@ -0,0 +1,190 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.complitex.pspoffice.information.strategy.region;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.interceptor.Interceptors;
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.util.string.Strings;
+import org.complitex.dictionaryfw.dao.StringCultureBean;
+import org.complitex.dictionaryfw.dao.aop.SqlSessionInterceptor;
+import org.complitex.dictionaryfw.entity.DomainObject;
+import org.complitex.dictionaryfw.entity.Attribute;
+import org.complitex.dictionaryfw.entity.description.EntityAttributeType;
+import org.complitex.dictionaryfw.entity.example.DomainObjectAttributeExample;
+import org.complitex.dictionaryfw.entity.example.DomainObjectExample;
+import org.complitex.dictionaryfw.strategy.Strategy;
+import org.complitex.dictionaryfw.strategy.web.DomainObjectEditPanel;
+import org.complitex.dictionaryfw.strategy.web.DomainObjectListPanel;
+import org.complitex.dictionaryfw.util.ResourceUtil;
+import org.complitex.dictionaryfw.web.component.search.ISearchCallback;
+import org.complitex.dictionaryfw.web.component.search.SearchComponent;
+import org.complitex.pspoffice.commons.web.pages.DomainObjectEdit;
+import org.complitex.pspoffice.commons.web.pages.DomainObjectList;
+
+/**
+ *
+ * @author Artem
+ */
+@Stateless
+@Interceptors({SqlSessionInterceptor.class})
+public class RegionStrategy extends Strategy {
+
+    @EJB
+    private StringCultureBean stringBean;
+
+    private static final long NAME_ATTRIBUTE_TYPE_ID = 700L;
+
+    @Override
+    public boolean isSimpleAttributeType(EntityAttributeType attributeDescription) {
+        return attributeDescription.getId() >= NAME_ATTRIBUTE_TYPE_ID;
+    }
+
+    @Override
+    public List<EntityAttributeType> getListColumns() {
+        return Lists.newArrayList(Iterables.filter(getEntity().getEntityAttributeTypes(), new Predicate<EntityAttributeType>() {
+
+            @Override
+            public boolean apply(EntityAttributeType attr) {
+                return attr.getId().equals(NAME_ATTRIBUTE_TYPE_ID);
+            }
+        }));
+    }
+
+    @Override
+    public String getEntityTable() {
+        return "region";
+    }
+
+    @Override
+    public String displayDomainObject(DomainObject object, Locale locale) {
+        return stringBean.displayValue(Iterables.find(object.getAttributes(), new Predicate<Attribute>() {
+
+            @Override
+            public boolean apply(Attribute attr) {
+                return attr.getAttributeTypeId().equals(NAME_ATTRIBUTE_TYPE_ID);
+            }
+        }).getLocalizedValues(), locale);
+    }
+
+    @Override
+    public ISearchCallback getSearchCallback() {
+        return new SearchCallback();
+    }
+
+    @Override
+    public void configureExample(DomainObjectExample example, Map<String, Long> ids, String searchTextInput) {
+        configureExampleImpl(example, ids, searchTextInput);
+    }
+
+    private static void configureExampleImpl(DomainObjectExample example, Map<String, Long> ids, String searchTextInput) {
+        if (!Strings.isEmpty(searchTextInput)) {
+            DomainObjectAttributeExample attrExample = null;
+            try {
+                attrExample = Iterables.find(example.getAttributeExamples(), new Predicate<DomainObjectAttributeExample>() {
+
+                    @Override
+                    public boolean apply(DomainObjectAttributeExample attrExample) {
+                        return attrExample.getAttributeTypeId().equals(NAME_ATTRIBUTE_TYPE_ID);
+                    }
+                });
+            } catch (NoSuchElementException e) {
+                attrExample = new DomainObjectAttributeExample(NAME_ATTRIBUTE_TYPE_ID);
+                example.addAttributeExample(attrExample);
+            }
+            attrExample.setValue(searchTextInput);
+        }
+        Long countryId = ids.get("country");
+        example.setParentId(countryId);
+        example.setParentEntity("country");
+    }
+
+    @Override
+    public List<String> getSearchFilters() {
+        return ImmutableList.of("country");
+    }
+
+    private static class SearchCallback implements ISearchCallback, Serializable {
+
+        @Override
+        public void found(SearchComponent component, Map<String, Long> ids, AjaxRequestTarget target) {
+            DomainObjectListPanel list = component.findParent(DomainObjectListPanel.class);
+            configureExampleImpl(list.getExample(), ids, null);
+            list.refreshContent(target);
+        }
+    }
+
+    @Override
+    public ISearchCallback getParentSearchCallback() {
+        return new ParentSearchCallback();
+    }
+
+    private static class ParentSearchCallback implements ISearchCallback, Serializable {
+
+        @Override
+        public void found(SearchComponent component, Map<String, Long> ids, AjaxRequestTarget target) {
+            DomainObjectEditPanel edit = component.findParent(DomainObjectEditPanel.class);
+            Long countryId = ids.get("country");
+            if (countryId != null && countryId > 0) {
+                edit.getObject().setParentId(countryId);
+                edit.getObject().setParentEntityId(800L);
+            } else {
+                edit.getObject().setParentId(null);
+                edit.getObject().setParentEntityId(null);
+            }
+        }
+    }
+
+    @Override
+    public Map<String, String> getChildrenInfo(Locale locale) {
+        String commonsBundle = "org.complitex.pspoffice.information.strategy.Commons";
+        return ImmutableMap.of("city", ResourceUtil.getString(commonsBundle, "city", locale));
+    }
+
+    @Override
+    public Class<? extends WebPage> getEditPage() {
+        return DomainObjectEdit.class;
+    }
+
+    @Override
+    public PageParameters getEditPageParams(Long objectId, Long parentId, String parentEntity) {
+        PageParameters params = new PageParameters();
+        params.put(DomainObjectEdit.ENTITY, getEntityTable());
+        params.put(DomainObjectEdit.OBJECT_ID, objectId);
+        params.put(DomainObjectEdit.PARENT_ID, parentId);
+        params.put(DomainObjectEdit.PARENT_ENTITY, parentEntity);
+        return params;
+    }
+
+    @Override
+    public Class<? extends WebPage> getListPage() {
+        return DomainObjectList.class;
+    }
+
+    @Override
+    public PageParameters getListPageParams() {
+        PageParameters params = new PageParameters();
+        params.put(DomainObjectList.ENTITY, getEntityTable());
+        return params;
+    }
+
+    @Override
+    public String[] getParents() {
+        return new String[]{"country"};
+    }
+}

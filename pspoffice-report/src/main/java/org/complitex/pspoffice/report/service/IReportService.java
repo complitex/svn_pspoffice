5c2fc6e2d8479a296ccc4e9c34ef71408eb538b1
heaven.iaa <heaven.iaa@38c073cf-b0f8-bd99-4753-81ba8f8cd33b>
2011-05-19 10:25:15 +0000

    add initial pspoffice report export module

---
+++ pspoffice-report/src/main/java/org/complitex/pspoffice/report/service/IReportService.java
@@ -0,0 +1,12 @@
+package org.complitex.pspoffice.report.service;
+
+import java.io.OutputStream;
+import java.util.Map;
+
+/**
+ * @author Anatoly A. Ivanov java@inheaven.ru
+ *         Date: 19.05.11 14:58
+ */
+public interface IReportService {
+    void createReport(String templateName, Map<String, String> parameters, OutputStream out) throws CreateReportException;
+}

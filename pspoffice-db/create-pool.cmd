a877e08ab3f29db1e10444c280cbec86baa28931
heaven.iaa <heaven.iaa@38c073cf-b0f8-bd99-4753-81ba8f8cd33b>
2011-06-21 09:50:30 +0000

    add create pspoffice pool

---
+++ pspoffice-db/create-pool.cmd
@@ -0,0 +1,19 @@
+@ECHO off
+
+SET GLASSFISH_ASADMIN=C:\glassfish3\bin\asadmin.bat
+
+ECHO ---------------------------------------------------
+ECHO Local database and Realm
+ECHO ---------------------------------------------------
+ECHO.
+ECHO Register the JDBC connection pool
+call %GLASSFISH_ASADMIN% create-jdbc-connection-pool --driverclassname com.mysql.jdbc.Driver --restype java.sql.Driver --property url=jdbc\:mysql\://localhost\:3306/pspoffice:user=pspoffice:password=pspoffice pspofficePool
+
+ECHO.
+ECHO Create a JDBC resource with the specified JNDI name
+call %GLASSFISH_ASADMIN% create-jdbc-resource --connectionpoolid pspofficePool jdbc/pspofficeResource
+
+ECHO.
+ECHO Add the named authentication realm
+call %GLASSFISH_ASADMIN% create-auth-realm --classname com.sun.enterprise.security.auth.realm.jdbc.JDBCRealm --property jaas-context=jdbcRealm:datasource-jndi=jdbc/pspofficeResource:user-table=user:user-name-column=login:password-column=password:group-table=usergroup:group-name-column=group_name:charset=UTF-8:digest-algorithm=MD5 pspofficeRealm
+

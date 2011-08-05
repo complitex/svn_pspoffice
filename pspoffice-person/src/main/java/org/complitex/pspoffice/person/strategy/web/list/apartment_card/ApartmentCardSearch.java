a90923eb31313f208cf756cead4270b0cdffdf63
artkof.work <artkof.work@38c073cf-b0f8-bd99-4753-81ba8f8cd33b>
2011-08-05 07:31:23 +0000

    Psp person work in progress. AddressToolbarButton was added into apartment card serach page.

--- pspoffice-person/src/main/java/org/complitex/pspoffice/person/strategy/web/list/apartment_card/ApartmentCardSearch.java
+++ pspoffice-person/src/main/java/org/complitex/pspoffice/person/strategy/web/list/apartment_card/ApartmentCardSearch.java
@@ -5,15 +5,19 @@
 package org.complitex.pspoffice.person.strategy.web.list.apartment_card;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Map;
 import javax.ejb.EJB;
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.authorization.strategies.role.annotations.AuthorizeInstantiation;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import static com.google.common.collect.ImmutableList.*;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.ResourceModel;
 import org.complitex.dictionary.entity.DomainObject;
 import org.complitex.dictionary.strategy.IStrategy.SimpleObjectInfo;
@@ -21,9 +25,11 @@ import org.complitex.dictionary.web.component.ShowMode;
 import org.complitex.dictionary.web.component.search.ISearchCallback;
 import org.complitex.dictionary.web.component.search.SearchComponentState;
 import org.complitex.dictionary.web.component.search.WiQuerySearchComponent;
+import org.complitex.dictionary.web.component.search.WiQuerySearchComponent.SearchFilterSettings;
 import org.complitex.pspoffice.person.strategy.ApartmentCardStrategy;
 import org.complitex.pspoffice.person.strategy.entity.ApartmentCard;
 import org.complitex.pspoffice.person.strategy.web.edit.apartment_card.ApartmentCardEdit;
+import org.complitex.template.web.component.toolbar.ToolbarButton;
 import org.complitex.template.web.security.SecurityRole;
 import org.complitex.template.web.template.FormTemplatePage;
 
@@ -33,6 +39,8 @@ import org.complitex.template.web.template.FormTemplatePage;
  */
 @AuthorizeInstantiation(SecurityRole.PERSON_MODULE_EDIT)
 public class ApartmentCardSearch extends FormTemplatePage {
+
+    private static final String SEARCH_COMPONENT_ID = "addressSearchComponent";
 
     private class AddressSearchCallback implements ISearchCallback, Serializable {
 
@@ -48,6 +56,8 @@ public class ApartmentCardSearch extends FormTemplatePage {
     private ApartmentCardStrategy apartmentCardStrategy;
     private final SearchComponentState addressSearchComponentState;
     private final FeedbackPanel messages;
+    private WebMarkupContainer addressSearchComponentContainer;
+    private IModel<Boolean> fullAddressEnabledModel;
 
     public ApartmentCardSearch() {
         add(new Label("title", new ResourceModel("title")));
@@ -56,11 +66,12 @@ public class ApartmentCardSearch extends FormTemplatePage {
         messages.setOutputMarkupId(true);
         add(messages);
 
-        addressSearchComponentState = new SearchComponentState();
-        WiQuerySearchComponent addressSearchComponent = new WiQuerySearchComponent("addressSearchComponent",
-                addressSearchComponentState, of("city", "street", "building", "apartment"), new AddressSearchCallback(),
-                ShowMode.ACTIVE, true);
-        add(addressSearchComponent);
+        addressSearchComponentState = apartmentCardStrategy.restoreSearchState(getTemplateSession());
+        addressSearchComponentContainer = new WebMarkupContainer("addressSearchComponentContainer");
+        addressSearchComponentContainer.setOutputMarkupId(true);
+        add(addressSearchComponentContainer);
+        WiQuerySearchComponent addressSearchComponent = newSearchComponent();
+        addressSearchComponentContainer.add(addressSearchComponent);
 
         AjaxLink<Void> submit = new AjaxLink<Void>("search") {
 
@@ -72,9 +83,29 @@ public class ApartmentCardSearch extends FormTemplatePage {
         add(submit);
     }
 
+    private WiQuerySearchComponent newSearchComponent() {
+        return fullAddressEnabledModel.getObject()
+                ? new WiQuerySearchComponent(SEARCH_COMPONENT_ID,
+                addressSearchComponentState, of("country", "region", "city", "street", "building", "apartment"), new AddressSearchCallback(),
+                ShowMode.ACTIVE, true)
+                : new WiQuerySearchComponent(SEARCH_COMPONENT_ID, addressSearchComponentState,
+                of(new SearchFilterSettings("country", false, false, ShowMode.ACTIVE),
+                new SearchFilterSettings("region", false, false, ShowMode.ACTIVE),
+                new SearchFilterSettings("city", false, false, ShowMode.ACTIVE),
+                new SearchFilterSettings("street", true, ShowMode.ACTIVE),
+                new SearchFilterSettings("building", true, ShowMode.ACTIVE),
+                new SearchFilterSettings("apartment", true, ShowMode.ACTIVE)), new AddressSearchCallback());
+    }
+
+    private void storeAdditionalInfo() {
+        apartmentCardStrategy.storeSearchState(getTemplateSession(), addressSearchComponentState);
+    }
+
     private void search(AjaxRequestTarget target) {
         SimpleObjectInfo addressInfo = getAddressObjectInfo();
         if (addressInfo != null) {
+            storeAdditionalInfo();
+
             String addressEntity = addressInfo.getEntityTable();
             long addressId = addressInfo.getId();
             int count = apartmentCardStrategy.countByAddress(addressEntity, addressId);
@@ -102,6 +133,22 @@ public class ApartmentCardSearch extends FormTemplatePage {
             return new SimpleObjectInfo("building", building.getId());
         }
         return new SimpleObjectInfo("apartment", apartment.getId());
+    }
+
+    @Override
+    protected List<? extends ToolbarButton> getToolbarButtons(String id) {
+        this.fullAddressEnabledModel = new Model<Boolean>(apartmentCardStrategy.isFullAddressEnabled(getTemplateSession()));
+        return of(new AddressToolbarButton(id, fullAddressEnabledModel.getObject()) {
+
+            @Override
+            protected void onClick(AjaxRequestTarget target) {
+                fullAddressEnabledModel.setObject(!fullAddressEnabledModel.getObject());
+                apartmentCardStrategy.storeFullAddressEnabled(getTemplateSession(), fullAddressEnabledModel.getObject());
+                addressSearchComponentContainer.replace(newSearchComponent());
+                target.addComponent(addressSearchComponentContainer);
+                target.addComponent(messages);
+            }
+        });
     }
 }
 

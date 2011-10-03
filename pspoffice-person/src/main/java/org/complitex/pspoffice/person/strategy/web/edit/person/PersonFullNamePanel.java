bb8a343b91b698bc556305c90431c2080a1f3157
artkof.work <artkof.work@38c073cf-b0f8-bd99-4753-81ba8f8cd33b>
2011-10-03 05:52:38 +0000

    Psp office work in progress. Persisting and loading of person names has been corrected to not save null values.

--- pspoffice-person/src/main/java/org/complitex/pspoffice/person/strategy/web/edit/person/PersonFullNamePanel.java
+++ pspoffice-person/src/main/java/org/complitex/pspoffice/person/strategy/web/edit/person/PersonFullNamePanel.java
@@ -16,6 +16,7 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.util.string.Strings;
 import org.complitex.dictionary.entity.Attribute;
 import org.complitex.dictionary.service.LocaleBean;
+import org.complitex.dictionary.web.component.DomainObjectInputPanel;
 import org.complitex.pspoffice.person.strategy.PersonStrategy;
 import org.complitex.pspoffice.person.strategy.entity.Person;
 import org.complitex.pspoffice.person.strategy.entity.PersonName.PersonNameType;
@@ -29,6 +30,8 @@ class PersonFullNamePanel extends Panel {
 
     @EJB
     private LocaleBean localeBean;
+    @EJB
+    private PersonStrategy personStrategy;
 
     PersonFullNamePanel(String id, Person person) {
         super(id);
@@ -43,32 +46,47 @@ class PersonFullNamePanel extends Panel {
 
     protected void init(Person person, final Locale defaultNameLocale, final String defaultLastName, final String defaultFirstName,
             final String defaultMiddleName) {
+        final IModel<String> lastNameLabelModel = newLabelModel(PersonStrategy.LAST_NAME);
+        add(new Label("lastNameLabel", lastNameLabelModel));
         add(new ListView<Attribute>("lastNames", person.getAttributes(PersonStrategy.LAST_NAME)) {
 
             @Override
             protected void populateItem(ListItem<Attribute> item) {
-                PersonFullNamePanel.this.populateItem(item, PersonNameType.LAST_NAME, "lastName", defaultNameLocale, defaultLastName);
+                PersonFullNamePanel.this.populateItem(item, PersonNameType.LAST_NAME, "lastName", lastNameLabelModel,
+                        defaultNameLocale, defaultLastName);
             }
         });
 
+        final IModel<String> firstNameLabelModel = newLabelModel(PersonStrategy.FIRST_NAME);
+        add(new Label("firstNameLabel", firstNameLabelModel));
         add(new ListView<Attribute>("firstNames", person.getAttributes(PersonStrategy.FIRST_NAME)) {
 
             @Override
             protected void populateItem(ListItem<Attribute> item) {
-                PersonFullNamePanel.this.populateItem(item, PersonNameType.FIRST_NAME, "firstName", defaultNameLocale, defaultFirstName);
+                PersonFullNamePanel.this.populateItem(item, PersonNameType.FIRST_NAME, "firstName", firstNameLabelModel,
+                        defaultNameLocale, defaultFirstName);
             }
         });
 
+        final IModel<String> middleNameLabelModel = newLabelModel(PersonStrategy.MIDDLE_NAME);
+        add(new Label("middleNameLabel", middleNameLabelModel));
         add(new ListView<Attribute>("middleNames", person.getAttributes(PersonStrategy.MIDDLE_NAME)) {
 
             @Override
             protected void populateItem(ListItem<Attribute> item) {
-                PersonFullNamePanel.this.populateItem(item, PersonNameType.MIDDLE_NAME, "middleName", defaultNameLocale, defaultMiddleName);
+                PersonFullNamePanel.this.populateItem(item, PersonNameType.MIDDLE_NAME, "middleName", middleNameLabelModel,
+                        defaultNameLocale, defaultMiddleName);
             }
         });
     }
 
+    private IModel<String> newLabelModel(long nameAttributeTypeId) {
+        return DomainObjectInputPanel.labelModel(personStrategy.getEntity().getAttributeType(nameAttributeTypeId).getAttributeNames(),
+                getLocale());
+    }
+
     private void populateItem(ListItem<Attribute> item, PersonNameType personNameType, String personNameComponentId,
+            IModel<String> labelModel,
             Locale defaultNameLocale, String defaultNameValue) {
         Attribute personNameAttribute = item.getModelObject();
         Locale locale = localeBean.getLocale(personNameAttribute.getAttributeId());
@@ -80,6 +98,7 @@ class PersonFullNamePanel extends Panel {
                 personNameType, locale, true)
                 : new PersonNameAutocompleteComponent(personNameComponentId, newNameModel(personNameAttribute), personNameType, locale, true);
         personNameComponent.setRequired(isSystemLocale);
+        personNameComponent.setLabel(labelModel);
         item.add(personNameComponent);
         Label language = new Label("language", locale.getDisplayLanguage(getLocale()));
         item.add(language);

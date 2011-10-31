5f82b3d9de6abac8ad451d59659ea8b3e23f7549
artkof.work <artkof.work@38c073cf-b0f8-bd99-4753-81ba8f8cd33b>
2011-10-31 10:06:11 +0000

    Psp office work in progress. Two new roles has been added - APARTMENT_EDIT and ROOM_EDIT so that apartment, room and person editing for plain employees and employees with extended privileges has been corrected.

--- pspoffice-person/src/main/java/org/complitex/pspoffice/person/strategy/web/component/AddressSearchPanel.java
+++ pspoffice-person/src/main/java/org/complitex/pspoffice/person/strategy/web/component/AddressSearchPanel.java
@@ -4,22 +4,15 @@
  */
 package org.complitex.pspoffice.person.strategy.web.component;
 
-import com.google.common.base.Function;
-import static com.google.common.collect.Lists.*;
 import java.util.List;
 import javax.ejb.EJB;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.complitex.admin.service.UserBean;
 import org.complitex.dictionary.entity.DomainObject;
-import org.complitex.dictionary.entity.User;
-import org.complitex.dictionary.entity.UserGroup;
-import org.complitex.dictionary.entity.UserOrganization;
 import org.complitex.dictionary.service.SessionBean;
 import org.complitex.dictionary.web.component.ShowMode;
 import org.complitex.dictionary.web.component.search.ISearchCallback;
 import org.complitex.dictionary.web.component.search.SearchComponentState;
-import org.complitex.dictionary.web.component.search.WiQuerySearchComponent;
 import org.complitex.dictionary.web.component.search.WiQuerySearchComponent.SearchFilterSettings;
 import org.complitex.pspoffice.person.strategy.web.component.autocomplete.EnhancedAddressSearchComponent;
 
@@ -32,24 +25,13 @@ public class AddressSearchPanel extends Panel {
     private static final String SEARCH_COMPONENT_ID = "searchComponent";
     @EJB
     private SessionBean sessionBean;
-    @EJB
-    private UserBean userBean;
 
     public AddressSearchPanel(String id, SearchComponentState searchComponentState, List<String> searchFilters,
             ISearchCallback callback, ShowMode showMode, boolean enabled) {
         super(id);
 
-        User user = getCurrentUser();
-        add(isEnhanced(user) ? new EnhancedAddressSearchComponent(SEARCH_COMPONENT_ID, searchComponentState, searchFilters,
-                callback, showMode, enabled, getUserOrganizationObjectIds(user)) {
-
-            @Override
-            protected void onUpdate(AjaxRequestTarget target, String entity) {
-                super.onUpdate(target, entity);
-                AddressSearchPanel.this.onUpdate(target, entity, getModelObject(entity));
-            }
-        }
-                : new WiQuerySearchComponent("searchComponent", searchComponentState, searchFilters, callback, showMode, enabled) {
+        add(new EnhancedAddressSearchComponent(SEARCH_COMPONENT_ID, searchComponentState, searchFilters,
+                callback, showMode, enabled, sessionBean.getUserOrganizationObjectIds()) {
 
             @Override
             protected void onUpdate(AjaxRequestTarget target, String entity) {
@@ -63,17 +45,8 @@ public class AddressSearchPanel extends Panel {
             List<SearchFilterSettings> searchFilterSettings, ISearchCallback callback) {
         super(id);
 
-        User user = getCurrentUser();
-        add(isEnhanced(user) ? new EnhancedAddressSearchComponent(SEARCH_COMPONENT_ID, searchComponentState,
-                searchFilterSettings, callback, getUserOrganizationObjectIds(user)) {
-
-            @Override
-            protected void onUpdate(AjaxRequestTarget target, String entity) {
-                super.onUpdate(target, entity);
-                AddressSearchPanel.this.onUpdate(target, entity, getModelObject(entity));
-            }
-        }
-                : new WiQuerySearchComponent("searchComponent", searchComponentState, searchFilterSettings, callback) {
+        add(new EnhancedAddressSearchComponent(SEARCH_COMPONENT_ID, searchComponentState,
+                searchFilterSettings, callback, sessionBean.getUserOrganizationObjectIds()) {
 
             @Override
             protected void onUpdate(AjaxRequestTarget target, String entity) {
@@ -81,37 +54,6 @@ public class AddressSearchPanel extends Panel {
                 AddressSearchPanel.this.onUpdate(target, entity, getModelObject(entity));
             }
         });
-    }
-
-    private User getCurrentUser() {
-        long userId = sessionBean.getCurrentUserId();
-        return userBean.getUser(userId);
-    }
-
-    private boolean isEnhanced(User user) {
-        List<UserGroup> userGroups = user.getUserGroups();
-        if (userGroups == null) {
-            return false;
-        }
-        boolean suit = false;
-        for (UserGroup group : userGroups) {
-            if (group.getGroupName().equals(UserGroup.GROUP_NAME.EMPLOYEES_CHILD_VIEW)
-                    || group.getGroupName().equals(UserGroup.GROUP_NAME.ADMINISTRATORS)) {
-                suit = true;
-                break;
-            }
-        }
-        return suit;
-    }
-
-    private List<Long> getUserOrganizationObjectIds(User user) {
-        return newArrayList(transform(user.getUserOrganizations(), new Function<UserOrganization, Long>() {
-
-            @Override
-            public Long apply(UserOrganization userOrganization) {
-                return userOrganization.getOrganizationObjectId();
-            }
-        }));
     }
 
     protected void onUpdate(AjaxRequestTarget target, String entity, DomainObject object) {

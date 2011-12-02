e998fe0457b47383d365e7dee24b57180c640687
artkof.work <artkof.work@38c073cf-b0f8-bd99-4753-81ba8f8cd33b>
2011-12-02 04:41:12 +0000

    Psp office work in progress. Common history page functionality has been taken away into AbstractHistoryPage.

---
+++ trunk/pspoffice-person/src/main/java/org/complitex/pspoffice/person/strategy/web/history/AbstractHistoryPage.java
@@ -0,0 +1,141 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.complitex.pspoffice.person.strategy.web.history;
+
+import java.util.Date;
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.IAjaxCallDecorator;
+import org.apache.wicket.ajax.calldecorator.AjaxCallDecorator;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.html.CSSPackageResource;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.util.string.Strings;
+import org.complitex.template.web.template.TemplatePage;
+import org.odlabs.wiquery.core.javascript.JsQuery;
+
+/**
+ *
+ * @author Artem
+ */
+public abstract class AbstractHistoryPage extends TemplatePage {
+
+    private static final String HISTORY_CONTENT_WICKET_ID = "historyContent";
+    private final WebMarkupContainer historyContainer;
+    private final long objectId;
+    private Date currentEndDate;
+
+    private abstract class HistoryLink extends AjaxLink<Void> {
+
+        private boolean isPostBack;
+        private final boolean initiallyVisible;
+
+        private HistoryLink(String id, boolean initiallyVisible) {
+            super(id);
+
+            this.initiallyVisible = initiallyVisible;
+        }
+
+        @Override
+        protected IAjaxCallDecorator getAjaxCallDecorator() {
+            return new AjaxCallDecorator() {
+
+                @Override
+                public CharSequence decorateScript(CharSequence script) {
+                    return "(function(){$(this).attr('disabled', true); $('#load_indicator').show();})();" + script;
+                }
+            };
+        }
+
+        @Override
+        public void onClick(AjaxRequestTarget target) {
+            String enableButtonScript = new JsQuery(this).$().render(false) + ".attr('disabled', false);";
+            target.appendJavascript("(function(){" + enableButtonScript + "$('#load_indicator').hide()})();");
+            historyContainer.replace(newHistoryContent(HISTORY_CONTENT_WICKET_ID, objectId, currentEndDate));
+            target.addComponent(historyContainer);
+            setHistoryButtonsVisibility(target);
+        }
+
+        @Override
+        protected void onComponentTag(ComponentTag tag) {
+            super.onComponentTag(tag);
+            if (!isPostBack) {
+                isPostBack = true;
+                if (!initiallyVisible) {
+                    String style = "";
+                    CharSequence styleCS = tag.getString("style");
+                    if (!Strings.isEmpty(styleCS)) {
+                        style = styleCS.toString();
+                    }
+                    if (!Strings.isEmpty(style) && !style.endsWith(";")) {
+                        style += ";";
+                    }
+                    tag.put("style", style + "visibility: hidden;");
+                }
+            }
+        }
+    }
+
+    public AbstractHistoryPage(final long objectId) {
+        this.objectId = objectId;
+
+        add(CSSPackageResource.getHeaderContribution(AbstractHistoryPage.class, AbstractHistoryPage.class.getSimpleName() + ".css"));
+
+        //history container and content.
+        historyContainer = new WebMarkupContainer("historyContainer");
+        historyContainer.setOutputMarkupId(true);
+        historyContainer.add(newHistoryContent(HISTORY_CONTENT_WICKET_ID, objectId, currentEndDate));
+        add(historyContainer);
+
+        //history
+        add(new HistoryLink("back", isBackButtonVisible()) {
+
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                Date newEndDate = getPreviousModificationDate(objectId, currentEndDate);
+                if (getPreviousModificationDate(objectId, newEndDate) != null) {
+                    currentEndDate = newEndDate;
+                    super.onClick(target);
+                }
+            }
+        });
+        add(new HistoryLink("forward", isForwardButtonVisible()) {
+
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                if (currentEndDate != null) {
+                    currentEndDate = getNextModificationDate(objectId, currentEndDate);
+                    super.onClick(target);
+                }
+            }
+        });
+    }
+
+    protected abstract Date getPreviousModificationDate(long objectId, Date currentEndDate);
+
+    protected abstract Date getNextModificationDate(long objectId, Date currentEndDate);
+
+    private boolean isBackButtonVisible() {
+        return getPreviousModificationDate(objectId,
+                getPreviousModificationDate(objectId, currentEndDate)) != null;
+    }
+
+    private boolean isForwardButtonVisible() {
+        return currentEndDate != null;
+    }
+
+    private void setHistoryButtonsVisibility(AjaxRequestTarget target) {
+        target.appendJavascript("(function(){ $('.history_back_button').css('visibility', '"
+                + getCssVisibility(isBackButtonVisible()) + "');"
+                + "$('.history_forward_button').css('visibility', '" + getCssVisibility(isForwardButtonVisible()) + "'); })()");
+    }
+
+    private String getCssVisibility(boolean visible) {
+        return visible ? "visible" : "hidden";
+    }
+
+    protected abstract Component newHistoryContent(String id, long objectId, Date currentEndDate);
+}

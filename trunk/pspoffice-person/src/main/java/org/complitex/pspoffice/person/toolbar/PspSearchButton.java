557c91357ac31c04729e5c53a90d9b64e55e7ea9
artkof.work <artkof.work@38c073cf-b0f8-bd99-4753-81ba8f8cd33b>
2012-03-12 08:05:19 +0000

    TemplateLoader has been fixed. New concept of application-wide tool bar buttons has been introduced and applied to pspoffice application. New role has been added - INFO_PANEL_ALLOWED - it permits to view information menu panel, for example, in pspoffice application plain employees have no rights to view information menu panel .

---
+++ trunk/pspoffice-person/src/main/java/org/complitex/pspoffice/person/toolbar/PspSearchButton.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.complitex.pspoffice.person.toolbar;
+
+import org.apache.wicket.ResourceReference;
+import org.complitex.pspoffice.person.menu.OperationMenu;
+import org.complitex.pspoffice.person.strategy.web.list.apartment_card.ApartmentCardSearch;
+import org.complitex.template.web.component.toolbar.ToolbarButton;
+import org.complitex.template.web.template.MenuManager;
+
+/**
+ *
+ * @author Artem
+ */
+public class PspSearchButton extends ToolbarButton {
+
+    private static final String IMAGE_SRC = "images/icon-open.gif";
+    private static final String TITLE_KEY = "psp_search_button";
+
+    public PspSearchButton(String id) {
+        super(id, new ResourceReference(IMAGE_SRC), TITLE_KEY);
+    }
+
+    @Override
+    protected void onClick() {
+        MenuManager.setMenuItem(OperationMenu.REGISTRATION_MENU_ITEM);
+        setResponsePage(ApartmentCardSearch.class);
+    }
+}

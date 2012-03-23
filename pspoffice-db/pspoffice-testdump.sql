80c2b8cca4615c82985e0ff0c25366e1784472f5
artkof.work <artkof.work@38c073cf-b0f8-bd99-4753-81ba8f8cd33b>
2012-03-23 10:31:32 +0000

    Toolbar buttons for adding new buildings, apartment and rooms has been added.

--- pspoffice-db/pspoffice-testdump.sql
+++ pspoffice-db/pspoffice-testdump.sql
@@ -29,7 +29,7 @@ insert into `organization_attribute`(`attribute_id`, `object_id`, `attribute_typ
 (1,2,900,3,900), (1,2,901,4,901), (1,2,904,1,904);
 
 -- Test users
--- User '1'
+-- Plain employee user '1'
 insert into `first_name` (`id`, `name`) values(3,'1');
 insert into `last_name` (`id`, `name`) values(3,'1');
 insert into `middle_name` (`id`, `name`) values(3,'1');
@@ -41,7 +41,21 @@ insert into `user_info_attribute` (`attribute_id`, `object_id`, `attribute_type_
 
 insert into `user` (`id`, `login`, `password`, `user_info_object_id`) values(3,'1','c4ca4238a0b923820dcc509a6f75849b',3);
 insert into `user_organization` (`id`, `user_id`, `organization_object_id`, `main`) values(1,3,1,1);
-insert into `usergroup` (`id`, `login`, `group_name`) values(4,1,'EMPLOYEES');
+insert into `usergroup` (`id`, `login`, `group_name`) values(4,'1','EMPLOYEES');
+
+-- Employee-child-view  user '2'
+insert into `first_name` (`id`, `name`) values(4,'2');
+insert into `last_name` (`id`, `name`) values(4,'2');
+insert into `middle_name` (`id`, `name`) values(4,'2');
+
+insert into `user_info` (`object_id`) values(4);
+insert into `user_info_attribute` (`attribute_id`, `object_id`, `attribute_type_id`, `value_id`, `value_type_id`) values(1,4,1000,4,1000);
+insert into `user_info_attribute` (`attribute_id`, `object_id`, `attribute_type_id`, `value_id`, `value_type_id`) values(1,4,1001,4,1001);
+insert into `user_info_attribute` (`attribute_id`, `object_id`, `attribute_type_id`, `value_id`, `value_type_id`) values(1,4,1002,4,1002);
+
+insert into `user` (`id`, `login`, `password`, `user_info_object_id`) values(4,'2','c81e728d9d4c2f636f067f89cc14862c',4);
+insert into `user_organization` (`id`, `user_id`, `organization_object_id`, `main`) values(2,4,1,1);
+insert into `usergroup` (`id`, `login`, `group_name`) values(5,'2','EMPLOYEES_CHILD_VIEW');
 
 --insert into config(`name`, `value`) values ('IMPORT_FILE_STORAGE_DIR', 'D:\\Artem\\Projects\\org.complitex\\storage\\import3');
 --insert into config(`name`, `value`) values ('DEFAULT_IMPORT_FILE_DIR', 'D:\\Artem\\Projects\\org.complitex\\pspoffice\\docs\\PSW_Rada');
--- pspoffice-person/src/main/java/org/complitex/pspoffice/person/strategy/web/component/ApartmentCreateDialog.java
+++ pspoffice-person/src/main/java/org/complitex/pspoffice/person/strategy/web/component/ApartmentCreateDialog.java
@@ -2,11 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.complitex.pspoffice.person.strategy.web.component.autocomplete;
+package org.complitex.pspoffice.person.strategy.web.component;
 
 import java.text.MessageFormat;
 import java.util.List;
 import javax.ejb.EJB;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.complitex.address.strategy.apartment.ApartmentStrategy;
 import org.complitex.address.strategy.apartment.web.edit.ApartmentEdit;
 import org.complitex.dictionary.entity.DomainObject;
@@ -16,13 +17,12 @@ import org.complitex.dictionary.service.LogBean;
 import org.complitex.dictionary.service.StringCultureBean;
 import org.complitex.dictionary.util.DateUtil;
 import org.complitex.pspoffice.person.Module;
-import org.odlabs.wiquery.ui.autocomplete.Autocomplete;
 
 /**
  *
  * @author Artem
  */
-abstract class ApartmentCreateDialog extends AbstractAddressCreateDialog {
+public abstract class ApartmentCreateDialog extends AbstractAddressCreateDialog {
 
     @EJB
     private StringCultureBean stringBean;
@@ -33,22 +33,26 @@ abstract class ApartmentCreateDialog extends AbstractAddressCreateDialog {
     @EJB
     private ApartmentStrategy apartmentStrategy;
 
-    ApartmentCreateDialog(String id, Autocomplete<String> autocomplete, List<Long> userOrganizationIds) {
-        super(id, autocomplete, userOrganizationIds);
+    protected ApartmentCreateDialog(String id, List<Long> userOrganizationIds) {
+        super(id, userOrganizationIds);
+    }
+
+    public void open(AjaxRequestTarget target, String number, DomainObject building) {
+        super.open(target, number, "building", building);
     }
 
     @Override
-    String getTitle() {
+    protected String getTitle() {
         return getString("apartment_title");
     }
 
     @Override
-    String getNumberLabel() {
+    protected String getNumberLabel() {
         return getString("number");
     }
 
     @Override
-    DomainObject initObject(String number) {
+    protected DomainObject initObject(String number) {
         DomainObject apartment = apartmentStrategy.newInstance();
         stringBean.getSystemStringCulture(apartment.getAttribute(ApartmentStrategy.NAME).getLocalizedValues()).setValue(number);
         apartment.setParentEntityId(ApartmentStrategy.PARENT_ENTITY_ID);
@@ -57,7 +61,7 @@ abstract class ApartmentCreateDialog extends AbstractAddressCreateDialog {
     }
 
     @Override
-    boolean validate(DomainObject object) {
+    protected boolean validate(DomainObject object) {
         Long existingObjectId = apartmentStrategy.performDefaultValidation(object, localeBean.getSystemLocale());
         if (existingObjectId != null) {
             error(MessageFormat.format(getString("validation_error"), existingObjectId));
@@ -66,7 +70,7 @@ abstract class ApartmentCreateDialog extends AbstractAddressCreateDialog {
     }
 
     @Override
-    DomainObject save(DomainObject object) {
+    protected DomainObject save(DomainObject object) {
         apartmentStrategy.insert(object, DateUtil.getCurrentDate());
         logBean.log(Log.STATUS.OK, Module.NAME, ApartmentCreateDialog.class,
                 Log.EVENT.CREATE, apartmentStrategy, null, object, null);
@@ -75,27 +79,27 @@ abstract class ApartmentCreateDialog extends AbstractAddressCreateDialog {
     }
 
     @Override
-    void bulkSave(DomainObject object) {
+    protected void bulkSave(DomainObject object) {
         apartmentStrategy.insert(object, DateUtil.getCurrentDate());
     }
 
     @Override
-    void beforeBulkSave(String numbers) {
+    protected void beforeBulkSave(String numbers) {
         ApartmentEdit.beforeBulkSave(Module.NAME, ApartmentCreateDialog.class, numbers, getLocale());
     }
 
     @Override
-    void afterBulkSave(String numbers, boolean operationSuccessed) {
+    protected void afterBulkSave(AjaxRequestTarget target, String numbers, boolean operationSuccessed) {
         ApartmentEdit.afterBulkSave(Module.NAME, ApartmentCreateDialog.class, numbers, operationSuccessed, getLocale());
     }
 
     @Override
-    void onFailBulkSave(DomainObject failObject, String numbers, String failNumber) {
+    protected void onFailBulkSave(AjaxRequestTarget target, DomainObject failObject, String numbers, String failNumber) {
         ApartmentEdit.onFailBulkSave(Module.NAME, ApartmentCreateDialog.class, failObject, numbers, failNumber, getLocale());
     }
 
     @Override
-    void onInvalidateBulkSave(DomainObject invalidObject, String numbers, String invalidNumber) {
+    protected void onInvalidateBulkSave(AjaxRequestTarget target, DomainObject invalidObject, String numbers, String invalidNumber) {
         ApartmentEdit.onInvalidateBulkSave(Module.NAME, ApartmentCreateDialog.class, invalidObject, numbers,
                 invalidNumber, getLocale());
     }

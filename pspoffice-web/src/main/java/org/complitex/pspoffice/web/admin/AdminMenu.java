56789dfc0093994472d2e6474346d9cf0ca3ec40
heaven.iaa <heaven.iaa@38c073cf-b0f8-bd99-4753-81ba8f8cd33b>
2012-07-06 12:01:02 +0000

    add report html menu link

--- pspoffice-web/src/main/java/org/complitex/pspoffice/web/admin/AdminMenu.java
+++ pspoffice-web/src/main/java/org/complitex/pspoffice/web/admin/AdminMenu.java
@@ -1,14 +1,15 @@
 package org.complitex.pspoffice.web.admin;
 
 import org.apache.wicket.Page;
-import org.complitex.template.web.template.ITemplateLink;
-
-import java.util.List;
-import java.util.Locale;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.complitex.admin.web.AdminTemplateMenu;
 import org.complitex.pspoffice.importing.legacy.web.LegacyDataImportPage;
 import org.complitex.pspoffice.importing.reference_data.web.ReferenceDataImportPage;
+import org.complitex.pspoffice.report.html.web.ReportList;
+import org.complitex.template.web.template.ITemplateLink;
+
+import java.util.List;
+import java.util.Locale;
 
 /**
  * @author Artem
@@ -62,6 +63,29 @@ public class AdminMenu extends AdminTemplateMenu {
             @Override
             public String getTagId() {
                 return "legacy_import";
+            }
+        });
+
+        links.add(new ITemplateLink() {
+
+            @Override
+            public String getLabel(Locale locale) {
+                return getString(ReportList.class, locale, "title");
+            }
+
+            @Override
+            public Class<? extends Page> getPage() {
+                return ReportList.class;
+            }
+
+            @Override
+            public PageParameters getParameters() {
+                return new PageParameters();
+            }
+
+            @Override
+            public String getTagId() {
+                return "report_html";
             }
         });
 

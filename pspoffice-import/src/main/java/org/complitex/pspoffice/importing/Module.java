3d29dd6597daa8683fb4090dcc899f8b3f73db6f
heaven.iaa <heaven.iaa@38c073cf-b0f8-bd99-4753-81ba8f8cd33b>
2012-09-27 06:28:13 +0000

    add payload import

--- pspoffice-import/src/main/java/org/complitex/pspoffice/importing/Module.java
+++ pspoffice-import/src/main/java/org/complitex/pspoffice/importing/Module.java
@@ -1,22 +1,10 @@
 package org.complitex.pspoffice.importing;
 
-import javax.annotation.PostConstruct;
-import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
-import org.complitex.dictionary.service.ConfigBean;
-import org.complitex.pspoffice.importing.legacy.entity.LegacyDataImportConfig;
 
 @Singleton(name = "PspImportModule")
 @Startup
 public class Module {
-
     public static final String NAME = "org.complitex.pspoffice.importing";
-    @EJB
-    private ConfigBean configBean;
-
-    @PostConstruct
-    public void init() {
-        configBean.init(LegacyDataImportConfig.class.getName(), LegacyDataImportConfig.values());
-    }
 }
